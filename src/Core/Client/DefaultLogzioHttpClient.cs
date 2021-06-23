9da8a3a3b485e4a87ebbff78375683f6d8a41a94
ShiranAvidov <shiran.avidov@logz.io>
2021-06-23 10:35:43 +0300

    added header

--- src/Core/Client/DefaultLogzioHttpClient.cs
+++ src/Core/Client/DefaultLogzioHttpClient.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using App.Metrics.Formatters;
@@ -51,15 +52,19 @@ namespace Core.Client
             {
                 var message = new HttpRequestMessage(HttpMethod.Post, _options.EndpointUri);
                 var byteArrayContent = new ByteArrayContent(payload, 0, payload.Length);
-                
+                var version = GetType().Assembly.GetName().Version;
+                string versionStr = version == null ? "1.0.0.0" : version.ToString();
+
                 message.Headers.Authorization = AuthenticationHeaderValue.Parse($"Bearer {_options.Token}");
                 byteArrayContent.Headers.Remove("Content-Type");
                 byteArrayContent.Headers.Add("Content-Type", mediaType.ContentType);
                 byteArrayContent.Headers.Remove("Content-Encoding");
                 byteArrayContent.Headers.Add("Content-Encoding", "snappy");
+                byteArrayContent.Headers.Add("Logzio-Shipper",
+                    $"logzio-app-metrics/v{versionStr}/{_failureAttempts}/0");
 
                 message.Content = byteArrayContent;
-                
+
                 var response = await _httpClient.SendAsync(message, cancellationToken);
 
                 if (!response.IsSuccessStatusCode)
@@ -84,7 +89,7 @@ namespace Core.Client
                 return new LogzioWriteResult(false, ex.ToString());
             }
         }
-        
+
         private bool NeedToBackoff()
         {
             if (Interlocked.Read(ref _failureAttempts) < _failuresBeforeBackoff)
